df2dcf4c5a7c93678f5c7d8fd5859c6106a7df35
Estefania Flores <eflores0227@gmail.com>
2016-02-04 14:03:22 -0800

    assignment 39 done

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,12 @@ class Post < ActiveRecord::Base
   belongs_to :topic
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :labelings, as: :labelable
+  has_many :labels, through: :labelings
 
   default_scope { order('created_at DESC') }
-
+  scope :ordered_by_title, -> { unscoped.order('title ASC') }
+  scope :ordered_by_reverse_created_at, -> { unscoped.order('created_at ASC')}
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
