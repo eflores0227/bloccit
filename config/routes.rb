691bac6b7b5139027ca4d10cd3ed92a3a82888a1
Estefania Flores <eflores0227@gmail.com>
2016-02-10 10:53:07 -0800

    assignment 48, done

--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:index, :show, :create, :update]
       resources :topics, except: [:edit, :new]
+      resources :posts, only: [:index, :show]
+      resouces :comments, only: [:index, :show]
     end
   end
 end
